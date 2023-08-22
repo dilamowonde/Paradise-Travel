d5d6f1e5705cc2211ba4281efc075e02ec98218d
dilamowonde <dilamo.wondimu@astu.edu.et>
2023-08-22 04:09:09 -0700

    intialised frontend with next.js

---
+++ Admin Dashboard/frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="">
+      <Button  variant="outline">Button</Button>
+
+      
+    </main>
+  )
+}
